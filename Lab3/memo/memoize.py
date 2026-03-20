1c8083c5637129b4e4a34e7303e576ca86f3a336
Alex <officialalexdemchenko@gmail.com>
2026-03-20 17:28:54 +0200

    Add cache size limit

--- Lab3/memo/memoize.py
+++ Lab3/memo/memoize.py
@@ -1,4 +1,4 @@
-def memoize(func):
+def memoize(func, max_size=None):
     cache = {}
 
     def wrapper(*args):
@@ -6,6 +6,11 @@ def memoize(func):
             return cache[args]
 
         result = func(*args)
+
+        if max_size and len(cache) >= max_size:
+            first_key = list(cache.keys())[0]
+            cache.pop(first_key)
+
         cache[args] = result
         return result
 
