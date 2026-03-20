d752370074411f15b5dc002c0cb7164c6a18f6e4
Alex <officialalexdemchenko@gmail.com>
2026-03-20 18:25:07 +0200

    Add LRU behavior test

--- Lab3/memo/main.py
+++ Lab3/memo/main.py
@@ -1,11 +1,11 @@
 from memoize import memoize
 
-@memoize(max_size=2)
+@memoize(max_size=2, policy="LRU")
 def square(x):
     print("Calculating...")
     return x * x
 
 print(square(1))
 print(square(2))
-print(square(3))
 print(square(1))
+print(square(3))
