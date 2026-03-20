18d0e058814a09727ef68e9684ee7b54afe43e04
Alex <officialalexdemchenko@gmail.com>
2026-03-20 18:47:32 +0200

    Add LFU behavior test

---
+++ Lab3/memo/LFU.py
@@ -0,0 +1,11 @@
+from memoize import memoize
+
+@memoize(max_size=2, policy="LFU")
+def square(x):
+    print("Calculating...")
+    return x * x
+
+print(square(1))  # freq: 1 -> 1
+print(square(2))  # freq: 2 -> 1
+print(square(1))  # freq: 1 -> 2
+print(square(3))  # freq: 3 -> 1 # Evicts 2 (LFU) since it has the lowest frequency (1) compared to 1 (2) and 3 (1)
