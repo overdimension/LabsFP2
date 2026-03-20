d9bacef7c09a46e61b663afcf822bd6832c0f19e
Alex <officialalexdemchenko@gmail.com>
2026-03-20 18:46:46 +0200

    Add LFU policy

--- Lab3/memo/memoize.py
+++ Lab3/memo/memoize.py
@@ -2,9 +2,11 @@ def memoize(max_size=None, policy="LRU"):
     def decorator(func):
         cache = {}
         order = []
+        freq = {}
 
         def wrapper(*args):
             if args in cache:
+                freq[args] += 1
                 if policy == "LRU":
                     order.remove(args)
                     order.append(args)
@@ -14,12 +16,19 @@ def memoize(max_size=None, policy="LRU"):
 
             if max_size and len(cache) >= max_size:
                 if policy == "LRU":
-                    oldest = order.pop(0)
-                    cache.pop(oldest)
+                    key_to_remove = order.pop(0) 
+                elif policy == "LFU":
+                    key_to_remove = min(freq, key=freq.get)
+                    if key_to_remove in order:
+                        order.remove(key_to_remove)
+                
+                cache.pop(key_to_remove)
+                freq.pop(key_to_remove)
 
             cache[args] = result
             order.append(args)
-            
+            freq[args] = 1
+
             return result
 
         return wrapper
