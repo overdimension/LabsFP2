468d7cff21c223d42e31dfcac59b502940104650
Alex <officialalexdemchenko@gmail.com>
2026-03-20 18:24:59 +0200

    Add LRU policy

--- Lab3/memo/memoize.py
+++ Lab3/memo/memoize.py
@@ -1,18 +1,25 @@
-def memoize(max_size=None):
+def memoize(max_size=None, policy="LRU"):
     def decorator(func):
         cache = {}
+        order = []
 
         def wrapper(*args):
             if args in cache:
+                if policy == "LRU":
+                    order.remove(args)
+                    order.append(args)
                 return cache[args]
 
             result = func(*args)
 
             if max_size and len(cache) >= max_size:
-                first_key = list(cache.keys())[0]
-                cache.pop(first_key)
+                if policy == "LRU":
+                    oldest = order.pop(0)
+                    cache.pop(oldest)
 
             cache[args] = result
+            order.append(args)
+            
             return result
 
         return wrapper
