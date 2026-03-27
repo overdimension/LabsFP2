d72e831ea86fc4e429f1756957ed294ce82e3c0d
Alex <officialalexdemchenko@gmail.com>
2026-03-27 15:34:14 +0200

    add basic manual  test for peek

--- Lab4/test_queue.py
+++ Lab4/test_queue.py
@@ -10,8 +10,16 @@ print("Stage 1:")
 print(pq.data)
 print()
 
+
+print("Stage 3:")
+print(pq.peek("highest"))
+print(pq.peek("lowest"))
+print(pq.peek("newest"))
+print(pq.peek("oldest"))
+print()
+
+
 print("Stage 2:")
 print(pq.dequeue("highest"))
 print(pq.dequeue("lowest"))
 print(pq.dequeue("oldest"))
-print()
