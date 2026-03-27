e9e3b682840881a097807c3d2384f60bd64ec160
Alex <officialalexdemchenko@gmail.com>
2026-03-27 15:27:37 +0200

    add basic manual test for dequeue

--- Lab4/test_queue.py
+++ Lab4/test_queue.py
@@ -3,7 +3,15 @@ from queue import PriorityQueue
 pq = PriorityQueue()
 
 pq.enqueue("A", 5)
+pq.enqueue("B", 1)
+pq.enqueue("C", 10)
 
 print("Stage 1:")
 print(pq.data)
+print()
+
+print("Stage 2:")
+print(pq.dequeue("highest"))
+print(pq.dequeue("lowest"))
+print(pq.dequeue("oldest"))
 print()
