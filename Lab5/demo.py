40f14fda6225ee1363bfef4c13da26f35445b9fd
Alex <officialalexdemchenko@gmail.com>
2026-03-30 16:38:30 +0300

    add async map test

--- Lab5/demo.py
+++ Lab5/demo.py
@@ -1,11 +1,14 @@
-from async_map import sync_map
+from async_map import sync_map, async_map
+import asyncio
 
 #Callback
 def square(x):
     return x * x
 
 #Async
-
+async def async_square(x):
+    await asyncio.sleep(0.5)
+    return x * x
 
 #Test
 def main():
@@ -13,5 +16,8 @@ def main():
 
     #Callback
     print("Callback result:", sync_map(square, numbers))
+
+    #Async
+    print("Async result:", await async_map(async_square, numbers))
 
 if __name__ == "__main__":  main()
