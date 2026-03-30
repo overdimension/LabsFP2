245b60baf991ea2bf98977a1b7d41b35d89603b8
Alex <officialalexdemchenko@gmail.com>
2026-03-30 16:38:08 +0300

    add async map version

--- Lab5/async_map.py
+++ Lab5/async_map.py
@@ -3,4 +3,8 @@ import asyncio
 #Callback
 def sync_map(func, iterable):
     return [func(x) for x in iterable]
+
 #Async
+async def async_map(func, iterable):
+    tasks = [func(x) for x in iterable]
+    return await asyncio.gather(*tasks)
