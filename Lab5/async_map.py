ce7c29dbc542d7cf0de77bff88c74b574adcf364
Alex <officialalexdemchenko@gmail.com>
2026-03-30 17:01:43 +0300

    add an abort version

--- Lab5/async_map.py
+++ Lab5/async_map.py
@@ -8,3 +8,12 @@ def sync_map(func, iterable):
 async def async_map(func, iterable):
     tasks = [func(x) for x in iterable]
     return await asyncio.gather(*tasks)
+
+#Abort
+async def async_map_with_abort(func, iterable):
+    tasks = [func(x) for x in iterable]
+    try:
+        return await asyncio.gather(*tasks)
+    except asyncio.CancelledError:
+        print("Cancelled!")
+        raise
