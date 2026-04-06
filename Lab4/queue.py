4e25da7f125d46c7fb11bb2f5485c8548fa37a39
Alex <officialalexdemchenko@gmail.com>
2026-04-06 11:31:01 +0300

    add basic dequeue logic

--- Lab4/queue.py
+++ Lab4/queue.py
@@ -15,3 +15,25 @@ def enqueue(self, item, priority):
     self.q.append((self.i, item))
     self.active.add(self.i)
     self.i += 1
+
+def dequeue(self, mode):
+    if not self.active:
+        return None
+
+    if mode == "lowest":
+        _, i, val = heapq.heappop(self.min_h)
+
+    elif mode == "highest":
+        _, i, val = heapq.heappop(self.max_h)
+
+    elif mode == "oldest":
+        i, val = self.q.popleft()
+
+    elif mode == "newest":
+        i, val = self.q.pop()
+
+    else:
+        raise ValueError
+
+    self.active.remove(i)
+    return val
