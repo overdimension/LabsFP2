c67cc903b3bc5c540890b088a5af2e6e21942dad
Alex <officialalexdemchenko@gmail.com>
2026-05-01 16:22:05 +0300

    add safety checks and unsubscribe functionality

--- Lab7/event_emitter.py
+++ Lab7/event_emitter.py
@@ -7,6 +7,11 @@ class EventEmitter:
             self.handlers[event] = []
         self.handlers[event].append(callback)
 
+        return lambda: self.handlers[event].remove(callback)
+
     def emit(self, event, data):
+        if event not in self.handlers:
+            return
+
         for handler in self.handlers[event]:
             handler(data)
